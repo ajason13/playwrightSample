9553fb3d59bfb5a003a389784af2295db29173bd
Jason Alvarez <a.jason13@gmail.com>
2024-04-09 15:41:06 -0700

    added page objects and partially refactored the first todo test

---
+++ pages/base.page.ts
@@ -0,0 +1,15 @@
+import type { Page, Locator } from '@playwright/test';
+
+export class BasePage {
+    page: Page;
+  
+    constructor(public readonly _page: Page) {
+      this.page = _page;
+    }
+  
+    async goto(partialUrl: string) {
+      await this.page.goto(partialUrl);
+    }
+
+    // Add common assertions here
+  }
