85ed3b09c749304b59069edac6bde1d0dde8700d
Jason Alvarez <a.jason13@gmail.com>
2024-04-11 14:50:14 -0700

    added a completed todo test

--- tests/todo.spec.ts
+++ tests/todo.spec.ts
@@ -62,6 +62,53 @@ test.describe('Todo page', () => {
     await expect(todoPage.todoItemText).toHaveText(TODO_ITEMS);
     await checkNumberOfTodosInLocalStorage(page, 3);
   });
+
+  test('should allow me to mark all items as completed', async ({ todoPage, page }) => {
+    await createDefaultTodos(todoPage);
+    await todoPage.markAllTodosAsComplete();
+
+    // Ensure all todos have 'completed' class.
+    await expect(todoPage.todoItems).toHaveClass([
+      'completed',
+      'completed',
+      'completed'
+    ]);
+    await checkNumberOfCompletedTodosInLocalStorage(page, 3);
+  });
+
+  // test('should allow me to clear the complete state of all items', async ({
+  //   page
+  // }) => {
+  //   const toggleAll = page.getByLabel('Mark all as complete');
+  //   // Check and then immediately uncheck.
+  //   await toggleAll.check();
+  //   await toggleAll.uncheck();
+
+  //   // Should be no completed classes.
+  //   await expect(page.getByTestId('todo-item')).toHaveClass(['', '', '']);
+  // });
+
+  // test('complete all checkbox should update state when items are completed / cleared', async ({
+  //   page
+  // }) => {
+  //   const toggleAll = page.getByLabel('Mark all as complete');
+  //   await toggleAll.check();
+  //   await expect(toggleAll).toBeChecked();
+  //   await checkNumberOfCompletedTodosInLocalStorage(page, 3);
+
+  //   // Uncheck first todo.
+  //   const firstTodo = page.getByTestId('todo-item').nth(0);
+  //   await firstTodo.getByRole('checkbox').uncheck();
+
+  //   // Reuse toggleAll locator and make sure its not checked.
+  //   await expect(toggleAll).not.toBeChecked();
+
+  //   await firstTodo.getByRole('checkbox').check();
+  //   await checkNumberOfCompletedTodosInLocalStorage(page, 3);
+
+  //   // Assert the toggle all is checked again.
+  //   await expect(toggleAll).toBeChecked();
+  // });
 });
 
 async function createDefaultTodos(todoPage: TodoPage) {
